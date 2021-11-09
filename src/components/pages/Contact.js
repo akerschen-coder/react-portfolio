1bab7e768ffb938a8592e17368e181fa277f4aec
Anastayzia Kerschen <anastayzia.kerschen@gmail.com>
2021-11-09 13:41:17 -0700

    did about and contact page - will need styling and further things done

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Contact() {
+    return (
+        <div>
+            <h1>Contact Page</h1>
+            <p>
+                <ul >
+                    <li> <a href="https://www.linkedin.com/in/anastayzia-kerschen-584744218/">LinkedIn</a></li>
+                    <li> <a href="https://github.com/akerschen-coder">Github</a> </li>
+                    <li> <a href="mailto:anastayzia.kerschen@gmail.com">Email</a> </li>
+                </ul>
+            </p>
+        </div>
+    );
+}
